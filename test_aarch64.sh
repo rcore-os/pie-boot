5fb96dc077ce6517b4fbe0bfb35271f3510a4cbc
周睿 <zrufo747@outlook.com>
2025-06-20 17:06:03 +0800

    fix: update dependencies in Cargo.lock and clean up test script

--- test_aarch64.sh
+++ test_aarch64.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-rm -rf target/aarch64-unknown-none-softfloat/debug/build/pie-boot-*
 # cargo test --target aarch64-unknown-none -p test-some-rt  --test test -- --show-output
 cargo test --target aarch64-unknown-none-softfloat -p test-some-rt --test test -- --show-output
