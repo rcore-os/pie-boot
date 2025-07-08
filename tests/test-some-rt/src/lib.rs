17a43e0392f69cc3b2b5b710f1a7ece0fd385e59
周睿 <zrufo747@outlook.com>
2025-07-08 14:56:04 +0800

    feat: update test configurations and add QEMU exit handling

--- tests/test-some-rt/src/lib.rs
+++ tests/test-some-rt/src/lib.rs
@@ -23,6 +23,9 @@ fn main(args: &BootInfo) -> ! {
 
     info!("All tests passed!");
 
+    #[cfg(feature = "qemu")]
+    exit();
+
     loop {
         spin_loop();
     }
@@ -38,4 +41,22 @@ fn clean_bss() {
             core::slice::from_raw_parts_mut(_sbss as *mut u8, _ebss as usize - _sbss as usize);
         bss.fill(0);
     }
+}
+
+#[cfg(feature = "qemu")]
+fn exit() {
+    use qemu_exit::QEMUExit;
+    #[cfg(target_arch = "aarch64")]
+    let qemu_exit_handle = qemu_exit::AArch64::new();
+
+    // addr: The address of sifive_test.
+    #[cfg(target_arch = "riscv64")]
+    let qemu_exit_handle = qemu_exit::RISCV64::new(addr);
+
+    // io_base:             I/O-base of isa-debug-exit.
+    // custom_exit_success: A custom success code; Must be an odd number.
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    let qemu_exit_handle = qemu_exit::X86::new(io_base, custom_exit_success);
+
+    qemu_exit_handle.exit_success();
 }
